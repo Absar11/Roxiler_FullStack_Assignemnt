f7abf54ac5382624efc2080bd89df9eac2c06edd
Absar11 <siddiqui.mr10@gmail.com>
2025-09-28 11:47:02 +0530

    feature: implement login functionality with JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,19 @@
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
 const userModel = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+require("dotenv").config;
+
+const generateToken = (id, role) => {
+    return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
 
 exports.register = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ message: errors.array()[0].msg });
+        return res.status(400).json({ 
+            message: errors.array()[0].msg 
+        });
     }
 
     const { name, email, password, address } = req.body;
@@ -13,7 +21,9 @@ exports.register = async (req, res) => {
     try {
         const existingUser = await userModel.findUserByEmail(email);
         if (existingUser) {
-            return res.status(409).json({ message: 'Email already registered.' });
+            return res.status(409).json({ 
+                message: 'Email already registered.' 
+            });
         }
 
         const salt = await bcrypt.genSalt(10);
@@ -37,6 +47,42 @@ exports.register = async (req, res) => {
 
     } catch (error) {
         console.error('Registration error:', error);
-        res.status(500).json({ message: 'Server error.' });
+        res.status(500).json({ 
+            message: 'Server error.' 
+        });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ 
+            message: 'Please provide email and password.' 
+        });
+    }
+
+    try {
+        const user = await userModel.findUserByEmail(email);
+
+        if (user && (await bcrypt.compare(password, user.password_hash))) {
+            const token = generateToken(user.user_id, user.role);
+            
+            res.json({
+                id: user.user_id,
+                name: user.name,
+                role: user.role,
+                token: token,
+            });
+        } else {
+            res.status(401).json({ 
+                message: 'Invalid credentials.' 
+            });
+        }
+    } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).json({
+             message: 'Server error.' 
+            });
     }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController');
 const { userValidation, validate } = require('../utils/validation');
 
 router.post('/register', userValidation, validate, authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
